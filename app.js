5ac95cb188b6cd53545a48d39f6ec34315b9c45e
aixancura <curaaixan4@gmail.com>
2023-11-10 03:15:21 -0300

    agregando html/css de login y modif.

--- app.js
+++ app.js
@@ -17,3 +17,15 @@ app.get("/", (req, res)=>{
 app.get("/register", (req,res) => {
     res.sendFile(path.resolve("./views/register.html"));
 })
+
+app.get("/login", (req,res) => {
+    res.sendFile(path.resolve("./views/login.html"));
+})
+
+app.get("/productDetail", (req,res) => {
+    res.sendFile(path.resolve("./views/productDetail.html"));
+})
+
+app.get("/productCart", (req,res) => {
+    res.sendFile(path.resolve("./views/productCart.html"));
+})
