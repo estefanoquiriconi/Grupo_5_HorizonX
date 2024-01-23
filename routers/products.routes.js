348c23bb0bb140d087294591643c0cdc720f86e4
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-01-23 11:51:39 -0300

    rutas de huéspedes y de usuarios, modifique algunas vistas para que solo se muestre algo si hay un usuario logueado y si es administrador

--- routers/products.routes.js
+++ routers/products.routes.js
@@ -4,22 +4,23 @@ const router = express.Router();
 const controller = require("../controllers/productsController");
 const uploadProduct = require('../middlewares/uploadProductMiddleware');
 const validateProduct = require('../middlewares/productFormValidationMiddleware');
+const authMiddleware = require("../middlewares/authMiddleware");
 
 router.get("/", controller.index);
 
 router.get("/detail/:id", controller.detail);
 
-router.get("/create", controller.create);
+router.get("/create", authMiddleware, controller.create);
 router.post("/create", uploadProduct.single('image'), validateProduct, controller.store);
 
-router.get("/edit/:id", controller.edit);
+router.get("/edit/:id", authMiddleware, controller.edit);
 router.put("/edit/:id", uploadProduct.single('image'), validateProduct, controller.update);
 
-router.get("/productCart", controller.productCart);
+router.get("/productCart", authMiddleware, controller.productCart);
 router.post("/buy", controller.buy);
 router.delete("/buy", controller.cartRemove);
 
-router.delete("/delete/:id",controller.delete);
+router.delete("/delete/:id", authMiddleware, controller.delete);
 
 
 module.exports = router;
