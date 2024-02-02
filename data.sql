5e42ec2a0e563f0b6d797ea7c67c7908a52d16e8
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-02-02 18:46:47 -0300

    data.sql

---
+++ data.sql
@@ -0,0 +1,22 @@
+INSERT INTO roles (name) VALUES
+('cliente'),
+('admin');
+
+INSERT INTO `colors` (`name`, `cod_hex`) VALUES
+  ('Negro', '#000000'),
+  ('Blanco', '#FFFFFF'),
+  ('Rojo', '#FF0000'),
+  ('Verde', '#00FF00'),
+  ('Azul', '#0000FF'),
+  ('Amarillo', '#FFFF00');
+
+INSERT INTO categories (name) VALUES
+('Celular'),
+('Accesorio')
+
+INSERT INTO brands (name) VALUES
+('Samsung'),
+('Motorola'),
+('Apple'),
+('Xiaomi'),
+('Huawei');
