ab4be607b962b29b835668362ec18f6e2a811995
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-02-03 11:03:18 -0300

    models: Product,ProductImage, Brand, Category, Color

---
+++ database/models/ProductImage.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+  const ProductImage = sequelize.define("ProductImage", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    image_filename: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+  });
+  return ProductImage;
+};
