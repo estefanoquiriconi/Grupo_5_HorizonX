1ef8f077841eb505a39bcdaeb3797b8c0aa20879
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-02-03 11:10:17 -0300

    migrations products

---
+++ database/migrations/product-images-create.js
@@ -0,0 +1,27 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("product_images", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      product_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "products",
+          key: "id",
+        },
+      },
+      image_filename: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("product_images");
+  },
+};
