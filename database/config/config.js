d2816cf3b1cabf639c139c4ecd4a2ad697342135
Soria Alvaro <alvarosoria.26@gmail.com>
2024-02-21 19:26:31 -0300

    modificacion del index,dimensionando imagenes, agregado efecto hover, modificado clase product en seccion product detail, dimensaionando correctamente la imagen y agregado un margin

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "Alvarosc35..",
+    "password": null,
     "database": "horizonx_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
