1680b9777c433de4e4cc569cef20e75928269c9d
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-03-03 14:58:44 -0300

    corrigiendo algunos estilos y la migración de cartItems

--- database/migrations/20241600-cartItems.create.js
+++ database/migrations/20241600-cartItems.create.js
@@ -19,14 +19,6 @@ module.exports = {
           type: Sequelize.INTEGER,
           allowNull: false,
         },
-        createdAt: {
-          type: Sequelize.DATE,
-          allowNull: false,
-        },
-        updatedAt: {
-          type: Sequelize.DATE,
-          allowNull: false,
-        },
       });
     },
   
