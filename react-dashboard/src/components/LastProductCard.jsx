ca3ceab5d96463f22a1c9ce77dde06d69b7108e5
eDaNi-M <danimiguel9626@gmail.com>
2024-04-04 16:47:52 -0300

    Vista detalles de producto react

--- react-dashboard/src/components/LastProductCard.jsx
+++ react-dashboard/src/components/LastProductCard.jsx
@@ -26,6 +26,7 @@ export const LastProductCard = () => {
   }, [])
 
   return (
+    <div className='container text-center'>
     <div className='card shadow mb-4'>
       <div className='card-header py-3'>
         <h5 className='m-0 font-weight-bold text-gray-800'>
@@ -35,28 +36,42 @@ export const LastProductCard = () => {
       <div className='card-body'>
         {loading ? (
           <p className='text-center'>Cargando...</p>
-        ) : error ? (
-          <p className='text-center text-danger'>{error}</p>
-        ) : (
+          ) : error ? (
+            <p className='text-center text-danger'>{error}</p>
+            ) : (
           <>
+            <h5 className="text-center">
             <strong>
               {product.brand.name} {product.name}
             </strong>
+          </h5>
             <div className='text-center'>
+            <Link to={`/product/${product.id}`}>
               <img
                 className='img-fluid px-3 px-sm-4 mt-3 mb-4'
                 style={{ width: '30rem' }}
                 src={product.images[0].url}
                 alt={product.images[0].image_filename}
-              />
+                />
+                </Link>
             </div>
-            <p>{product.description}</p>
-            <Link className='btn btn-primary' to='/product/last'>
+            <em className="text-center">{product.description}</em>
+              <br />
+              <br />
+              <p className="text-center">Color: {product.color.name}</p>
+              <p className="text-center">En stock: {product.stock_quantity}</p>
+              <br />
+              <h3 className="text-center">
+                <strong>$ {product.price}</strong>
+              </h3>
+              <br />
+            {/* <Link className='btn btn-primary' to='/product/last'>
               Ver producto
-            </Link>
+            </Link> */}
           </>
         )}
       </div>
     </div>
+        </div>
   )
 }
--- react-dashboard/src/components/ProductDetail.jsx
+++ react-dashboard/src/components/ProductDetail.jsx
@@ -1,18 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export const ProductDetail = () => {
   const { id } = useParams();
-  const [product, setProduct] = useState ([]);
+  const [product, setProduct] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const response = await fetch(`http://localhost:8080/api/products/${id}`);
+        const response = await fetch(
+          `http://localhost:8080/api/products/${id}`
+        );
         if (!response.ok) {
-          throw new Error('Error al obtener los datos del producto');
+          throw new Error("Error al obtener los datos del producto");
         }
         const data = await response.json();
         setProduct(data);
@@ -31,7 +33,7 @@ export const ProductDetail = () => {
   }
 
   if (error) {
-    return <p className='text-danger'>{error}</p>;
+    return <p className="text-danger">{error}</p>;
   }
 
   if (!product) {
@@ -39,28 +41,43 @@ export const ProductDetail = () => {
   }
 
   return (
-    <div className='card-body'>
-    {loading ? (
-      <p className='text-center'>Cargando...</p>
-    ) : error ? (
-      <p className='text-center text-danger'>{error}</p>
-    ) : (
-      <>
-        <strong>
-          {product.brand.name} {product.name}
-        </strong>
-        <div className='text-center'>
-          <img
-            className='img-fluid px-3 px-sm-4 mt-3 mb-4'
-            style={{ width: '30rem' }}
-            src={product.images[0].url}
-            alt={product.images[0].image_filename}
-          />
+    <div className="container text-center">
+      <div className="card shadow mb-4">
+        <div className="card-header py-3">
+          <h5>
+            <strong>
+              {product.brand.name} {product.name}
+            </strong>
+          </h5>
         </div>
-        <p>{product.description}</p>
-      
-      </>
-    )}
-  </div>
+        <div className="card-body">
+          {loading ? (
+            <p className="text-center">Cargando...</p>
+          ) : error ? (
+            <p className="text-center text-danger">{error}</p>
+          ) : (
+            <>
+              <div className="text-center">
+                <img
+                  className="img-fluid px-3 px-sm-4 mt-3 mb-4"
+                  style={{ width: "30rem" }}
+                  src={product.images[0].url}
+                  alt={product.images[0].image_filename}
+                />
+              </div>
+              <em className="text-center">{product.description}</em>
+              <br />
+              <br />
+              <p className="text-center">Color: {product.color.name}</p>
+              <p className="text-center">En stock: {product.stock_quantity}</p>
+              <br />
+              <h3 className="text-center">
+                <strong>$ {product.price}</strong>
+              </h3>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
