6f596bb5d43d0d91f08274bfcfc3b3693c5329b4
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-04-06 19:45:42 -0300

    Correciones

--- react-dashboard/src/components/Brands.jsx
+++ react-dashboard/src/components/Brands.jsx
@@ -1,59 +1,4 @@
-/* import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-
-export const Brands = () => {
-    const [brand, setCategories] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
-
-    const getCategories = async () => {
-        try {
-            const response = await fetch('http://localhost:8080/api/brands')
-            if (!response.ok) {
-                throw new Error('Error al obtener los datos')
-            }
-            const data = await response.json()
-            setCategories(data.data)
-        } catch (error) {
-            setError(error.message)
-        } finally {
-            setLoading(false)
-        }
-    }
-
-    useEffect(() => {
-        getCategories()
-    }, [])
-
-    return (
-        <div className='container text-center'>
-        <div className='card shadow mb-4'>
-          <div className='card-header py-3'>
-            <h5 className='m-0 font-weight-bold text-gray-800'>
-              Marcas
-            </h5>
-          </div>
-          <div className='card-body'>
-            {loading ? (
-              <p className='text-center'>Cargando...</p>
-              ) : error ? (
-                <p className='text-center text-danger'>{error}</p>
-                ) : (
-              <>
-                <h5 className="text-center">
-                <strong>
-                  {product.brand.name} {brand.name}
-                </strong>
-              </h5>
-              </>
-            )}
-          </div>
-        </div>
-            </div>
-      )
-    }*/
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 
 export const Brands = () => {
     const [brands, setBrands] = useState(null);
@@ -65,7 +10,6 @@ export const Brands = () => {
             const fetchBrands = await fetch('http://localhost:8080/api/brands')
             const dataBrands = await fetchBrands.json()
             const brands = dataBrands.data
-            //console.log(brands)
 
             if (!brands) {
                 throw new Error('No se encontró ninguna marca.');
